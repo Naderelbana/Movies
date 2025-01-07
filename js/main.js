adba1623a2771c953ee9d99cc6d0cb197f5055c2
Naderelbana <nader.mido63@gmail.com>
2025-01-07 19:10:55 +0200

    end of movies sections

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-const scrollContainer = document.getElementById('scrollable');
-    const scrollLeftBtn = document.getElementById('scroll-left');
-    const scrollRightBtn = document.getElementById('scroll-right');
+    // التعامل مع جميع الأزرار باستخدام الحدث العام
+    document.addEventListener('click', function (e) {
+      const btn = e.target.closest('.scroll-btn');
+      if (btn) {
+        const targetId = btn.getAttribute('data-target');
+        const direction = btn.getAttribute('data-scroll');
+        const scrollContainer = document.getElementById(targetId);
 
-    // التمرير لليسار عند النقر على السهم الأيسر
-    scrollLeftBtn.addEventListener('click', () => {
-      scrollContainer.scrollBy({
-        left: -300, // تمرير للخلف بمقدار 300 بكسل
-        behavior: 'smooth'
-      });
-    });
-
-    // التمرير لليمين عند النقر على السهم الأيمن
-    scrollRightBtn.addEventListener('click', () => {
-      scrollContainer.scrollBy({
-        left: 300, // تمرير للأمام بمقدار 300 بكسل
-        behavior: 'smooth'
-      });
+        if (scrollContainer) {
+          scrollContainer.scrollBy({
+            left: direction === 'left' ? -200 : 200, // اتجاه التمرير
+            behavior: 'smooth',
+          });
+        }
+      }
     });
